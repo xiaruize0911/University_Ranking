69aa1d7ec26c0e1a351b65a3eebd07d836dbde1d
xiaruize <2188298460@qq.com>
2025-07-04 03:53:43 +0800

    upd

---
+++ University_rankings.session.sql
@@ -0,0 +1,10 @@
+CREATE TABLE Universities (
+    id INTEGER PRIMARY KEY,
+    normalized_name text,
+    name text,
+    country text,
+    country_code text,
+    city text,
+    photo text,
+    blurb text
+);
